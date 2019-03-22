d5cf6e33a37bb8fab241caadb06cf35729caf3d0
Brian Illes <brianrilles@gmail.com>
2019-03-22 09:23:22 -0700

    adds CRUD operations ability on actions

---
+++ projects/projects-router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const Projects = require('../data/helpers/projectModel');
+
+const router = express.Router();
+
+// handles URLS beginning with /api/projects
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const actionsRouter = require('./actions/actions-router.js');
+const projectsRouter = require('./projects/projects-router.js');
+
 const server = express();
 
 server.use(express.json());
@@ -7,5 +10,9 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.send(`<h1>Welcome<h1>`);
 });
+
+// routing
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
 
 module.exports = server;
