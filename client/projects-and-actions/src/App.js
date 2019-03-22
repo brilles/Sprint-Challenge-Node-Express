ef60ffff69c8ca44f20d638539704404258a1d50
Brian Illes <brianrilles@gmail.com>
2019-03-22 14:02:25 -0700

    styling

--- client/projects-and-actions/src/App.js
+++ client/projects-and-actions/src/App.js
@@ -12,6 +12,44 @@ function Project({ project, id, deleteProject }) {
   );
 }
 
+function FormAddOrUpdateProject({ addProject }) {
+  const [project, setProject] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addProject({
+      name: project,
+      description
+    });
+    setProject('');
+    setDescription('');
+  };
+
+  return (
+    <div className="add-or-update-form">
+      <h3>Add Project:</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Title"
+          value={project}
+          onChange={e => setProject(e.target.value)}
+        />{' '}
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />{' '}
+        <button>{'Add Project'}</button>
+      </form>
+    </div>
+  );
+}
+
 export default function App() {
   const [projects, setProjects] = useState([]);
 
@@ -39,17 +77,34 @@ export default function App() {
   useEffect(() => {
     fetchData();
   }, []);
+
+  const addProject = project => {
+    axios
+      .post('http://localhost:4000/api/projects', project)
+      .then(res => {
+        fetchData();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="app">
       <h1>Projects: </h1>
-      {projects.map(project => (
-        <Project
-          project={project}
-          id={project.id}
-          key={project.id}
-          deleteProject={deleteProject}
-        />
-      ))}
+      <div className="main-content">
+        <div className="projects">
+          {projects.map(project => (
+            <Project
+              project={project}
+              id={project.id}
+              key={project.id}
+              deleteProject={deleteProject}
+            />
+          ))}
+        </div>
+        <FormAddOrUpdateProject addProject={addProject} />
+      </div>
     </div>
   );
 }
